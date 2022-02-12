75ff466f91ffcf36fe9cc8c2a2db06d6b57799e5
Santfogu <79651297+Santfogu@users.noreply.github.com>
2022-02-12 15:18:47 -0500

    Subida a Git

---
+++ Taller 1/src/Modelo/ProductoMenu.java
@@ -0,0 +1,41 @@
+package Modelo;
+
+public class ProductoMenu implements Producto{
+
+	
+	
+	
+	private String nombre;
+	
+	private int precioBase;
+	
+	
+	
+	public ProductoMenu (String nombre, int precioBase) 
+	{
+		this.nombre = nombre;
+		this.precioBase = precioBase;
+	}
+	
+	
+
+
+	
+	public int getPrecio() {
+		return this.precioBase;
+	}
+
+
+	
+	public String getNombre() {
+		return this.nombre;
+	}
+
+
+	
+	public String generarTextoFactura() {
+		return nombre+" : $"+precioBase;
+	}
+
+
+}

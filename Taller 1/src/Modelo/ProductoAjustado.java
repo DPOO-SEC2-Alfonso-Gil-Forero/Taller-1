ab1c7bf39959d1c748e378ed75a09fa6ca4fbe12
Santfogu <79651297+Santfogu@users.noreply.github.com>
2022-02-13 00:47:02 -0500

    Producto ajustado modificación

--- Taller 1/src/Modelo/ProductoAjustado.java
+++ Taller 1/src/Modelo/ProductoAjustado.java
@@ -1,5 +1,73 @@
 package Modelo;
 
-public class ProductoAjustado {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductoAjustado implements Producto
+{
+
+	
+	private ProductoMenu base;
+	
+	private List<Ingrediente> agregados;
+	
+	private List<Ingrediente> eliminados;
+	
+	
+	
+	//Constructor
+	
+	public ProductoAjustado(ProductoMenu base) 
+	{
+		this.base = base;
+		agregados = new ArrayList<Ingrediente>();
+		eliminados = new ArrayList<Ingrediente>();
+	}
+	
+	
+	
+	//Métodos
+	
+	public int getPrecio() {
+		int precioBase = base.getPrecio();
+		int precioExtra = 0;
+		for (Ingrediente productoExtra : agregados) 
+		{
+			precioExtra += productoExtra.getCostoAdicional();
+		}
+		return precioBase + precioExtra;
+	}
+
+	public String getNombre() 
+	{
+		String cadena = base.getNombre();
+		for (Ingrediente productoExtra : agregados) 
+		{
+			cadena += "Adición de : "+productoExtra.getNombre();
+		}
+		for (Ingrediente productoExtra : eliminados) 
+		{
+			cadena += "Sin : "+productoExtra.getNombre();
+		}
+		return cadena;
+	}
+
+	public String generarTextoFactura() 
+	{
+		return base.generarTextoFactura();
+	}
+
+
+	public void agregarIngrediente(Ingrediente nuevoIngrediente) 
+	{
+		agregados.add(nuevoIngrediente);
+	}
+
+
+
+	public void eliminarIngrediente(Ingrediente nuevoIngrediente) 
+	{
+		eliminados.add(nuevoIngrediente);
+	}
 
 }
